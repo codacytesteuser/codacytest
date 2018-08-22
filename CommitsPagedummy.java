cdc47e3f4d0689acd4cf17c976d4172a495090e0
codacytesteuser <38557762+codacytesteuser@users.noreply.github.com>
2018-08-22 15:24:52 +0100

    Update CommitsPagedummy.java

--- CommitsPagedummy.java
+++ CommitsPagedummy.java
@@ -10,6 +10,7 @@ novas cenas
 import java.lang.management.GarbageCollectorMXBean;
 
 public class CommitsPage extends BasePage {
+    private static final String COMMIT_TABLE2 = "//*[@class=\"pull-left col-xs-12 files-wrapper2\"]";
     private static final String COMMIT_TABLE = "//*[@class=\"pull-left col-xs-12 files-wrapper\"]";
     private static final String COLUMN_STATUS = "status_column";
     private static final String COLUMN_AUTHOR = "status_author";
