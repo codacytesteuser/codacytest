11656ad13396aca465d750b96b331062736cc666
Bruno Medley <brunomedley@gmail.com>
2018-09-12 17:59:14 +0100

    other one

--- CommitsPagedummy.java
+++ CommitsPagedummy.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.lang.management.GarbageCollectorMXBean;
 
 public class CommitsPage extends BasePage {
+    private static final String COMMIT_TABLE23 = "//*[@class=\"pull-left col-xs-12 files-wrapper4\"]";
     private static final String COMMIT_TABLE22 = "//*[@class=\"pull-left col-xs-12 files-wrapper4\"]";
     private static final String COMMIT_TABLE21 = "//*[@class=\"pull-left col-xs-12 files-wrapper4\"]";
     private static final String COMMIT_TABLE20 = "//*[@class=\"pull-left col-xs-12 files-wrapper4\"]";
